bbdfb06c0dbee511dcdd9f0a46d20d9ba9129b18
George Stagas <gstagas@gmail.com>
2013-01-27 03:35:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# keyboard
+
+keyboard system
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,53 @@
+
+/**
+ * Keyboard map.
+ */
+
+var map = {
+  37: 'left'
+, 38: 'up'
+, 39: 'right'
+, 40: 'down'
+}
+
+/**
+ * keyboard
+ */
+
+module.exports = function (el) {
+  var kbd = {}
+
+  kbd.keys = {}
+
+  kbd.init = function () {
+
+    // add listeners
+
+    el.addEventListener('keydown', function (ev) {
+      ev.preventDefault()
+      ev.stopPropagation()
+      kbd.keys[ev.which] = true
+      kbd.emit('keys', kbd.getKeys())
+      return false
+    })
+
+    el.addEventListener('keyup', function (ev) {
+      ev.preventDefault()
+      ev.stopPropagation()
+      kbd.keys[ev.which] = false
+      kbd.emit('keys', kbd.getKeys())
+      return false
+    })
+    
+  }
+
+  kbd.getKeys = function () {
+    return Object.keys(kbd.keys)
+      .filter(function (el) { return kbd.keys[el] }, this)
+      .sort(function (a, b) { return a - b })
+      .map(function (el) { return map[el] })
+      .join(' ')
+  }
+
+  return kbd
+}
